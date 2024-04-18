cb3ae83dcfb9ca913efa0d4f28d524c0c364131a
three <xtygah14three@hotmail.com>
2024-04-18 09:40:10 +0800

    Changes

--- data/net.go
+++ data/net.go
@@ -1,7 +1,17 @@
 package data
 
+import "encoding/json"
+
 type NetResult struct {
 	Success bool        `json:"success,omitempty"`
 	Data    interface{} `json:"data,omitempty"`
 	Msg     string      `json:"msg,omitempty"`
+}
+
+func (r *NetResult) ToBytes() []byte {
+	marshal, err := json.Marshal(r)
+	if err != nil {
+		return nil
+	}
+	return marshal
 }
